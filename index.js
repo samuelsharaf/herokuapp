ef9b380a0989680be6f668f1f3b3168c01fc8c27
Samuel Sharaf <samuel.sharaf@salesforce.com>
2014-12-02 12:52:20 -0800

    removed pg

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
