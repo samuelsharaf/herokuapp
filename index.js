d6b72bb7a0bde84fdeadde7ddad44a243135b9f8
Samuel Sharaf <samuel.sharaf@salesforce.com>
2014-12-04 13:06:47 -0800

    dfsdf

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var pg = require('pg');
+
 app.use(express.static(__dirname + '/images'));
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) {
   //response.send('Hello World!');
